6e5f950c7e1ddf962f89141133a2ea41032b1b59
CyberCatOfUkraine <22132911+CyberCatOfUkraine@users.noreply.github.com>
2022-12-23 23:34:34 +0200

    Add project files.

---
+++ ApiScraper/Scraper/WebScraper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiScraper.Scrapper
+{
+    internal class WebScraper
+    {
+        private static readonly HttpClient client = new HttpClient();
+        public async Task<string> GetTextByUrl(string url)
+        {
+            var result = string.Empty;
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                result = await response.Content.ReadAsStringAsync();
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+            return result;
+        }
+
+    }
+}
---
+++ Console/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ApiScraper.Scrapper;
+
+Console.WriteLine("Hello, World!");
+
+
+CoinCapScraper scraper = new();
+scraper.Initialize();
+
+List<ApiScraper.Models.CryptocurrencyDetails> all = scraper.GetAll;
+
+Console.WriteLine(all.Count);
+
+var first = all[0];
+var details = scraper.Get(first.Name);
+Console.WriteLine(
+    details.Name + " : " +
+    details.Code + " : " +
+    details.Price + " : " +
+    details.BaseId + " : " +
+    details.Volume + " : " +
+    details.PriceChange
+    );
+Console.ReadKey();
+
+
